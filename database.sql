9acb293f88f288e929beb998ed87678fef9dfa6b
iFree <weizhuang_l@163.com>
2016-08-24 01:05:00 +0800

    database.sql

--- database.sql
+++ database.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : LOCAL_CENTOS
-Source Server Version : 50549
-Source Host           : 192.168.1.253:3306
+Source Server         : local_b_db
+Source Server Version : 50632
+Source Host           : 192.168.1.22:3306
 Source Database       : publish
 
 Target Server Type    : MYSQL
-Target Server Version : 50549
+Target Server Version : 50632
 File Encoding         : 65001
 
-Date: 2016-08-23 21:30:04
+Date: 2016-08-24 01:04:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,13 +22,16 @@ DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `uid` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
-  `trunk` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '分支',
-  `checkout` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '检出路径',
-  `export` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '导出路径',
+  `trunk` varchar(255) DEFAULT NULL COMMENT '分支',
+  `checkout` varchar(255) DEFAULT NULL COMMENT '检出路径',
+  `export` varchar(255) DEFAULT NULL COMMENT '导出路径',
+  `remote_host` varchar(255) DEFAULT NULL COMMENT '目标机器',
+  `remote_user` varchar(255) DEFAULT NULL COMMENT '目标用户',
+  `excludes` varchar(255) DEFAULT NULL COMMENT '忽略文件',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for task
@@ -38,8 +41,9 @@ CREATE TABLE `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `uid` int(11) DEFAULT NULL COMMENT '用户ID',
   `projectId` int(11) DEFAULT NULL COMMENT '项目ID',
-  `title` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '标题',
+  `title` varchar(255) DEFAULT NULL COMMENT '标题',
   `status` int(11) DEFAULT NULL COMMENT '状态',
-  `branches` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '发布分支',
+  `branches` varchar(255) DEFAULT NULL COMMENT '发布分支',
+  `errorMsg` varchar(255) DEFAULT NULL COMMENT '错误信息',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
